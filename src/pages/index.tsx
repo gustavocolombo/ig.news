7ce20bc10a9240e049ca2190647d81c8a967045f
Gustavo Colombo <gusttavocolombo@gmail.com>
2022-01-04 18:18:09 -0300

    finish page home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,8 @@
+/* eslint-disable @next/next/no-img-element */
 import Head from "next/head";
+import styles from './home.module.scss';
 import { Fragment } from "react";
+import SubscribeButton from "../components/SubcribeButton";
 
 export default function Home() {
   return (
@@ -7,7 +10,18 @@ export default function Home() {
       <Head>
         <title>Página inicial</title>
       </Head>
-      <h1>Hello world</h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome!</span>
+          <h1>News about the <span>React</span> world</h1>
+          <p>
+            Get access to all the publications <br/>
+            <span>for $9,90</span>
+          </p>
+          <SubscribeButton/>
+        </section>
+        <img src="/images/avatar.svg" alt="logo" />
+      </main>
     </Fragment>
   )
 }
