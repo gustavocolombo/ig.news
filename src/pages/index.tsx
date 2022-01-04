507df05fb6db21b9a16109e06bb9f85ca3c21fbd
Gustavo Colombo <gusttavocolombo@gmail.com>
2022-01-04 16:07:59 -0300

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import { Fragment } from "react";
+
+export default function Home() {
+  return (
+    <Fragment>
+      <Head>
+        <title>Página inicial</title>
+      </Head>
+      <h1>Hello world</h1>
+    </Fragment>
+  )
+}
