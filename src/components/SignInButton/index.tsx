5abbbdcfbfdf34c9d87d35adcde79d1fb08dc6af
Gustavo Colombo <gusttavocolombo@gmail.com>
2022-01-06 19:59:24 -0300

    faunadb insert user

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,16 +1,20 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
-import { useSession, signIn } from 'next-auth/react'
+import { useSession, signIn, signOut } from 'next-auth/react'
 import styles from './styles.module.scss'
 
 export default function SignInButton(){
 
-  const {data: session} = useSession();
+  const { data: session } = useSession();
 
   return session ? (
-    <button type="button" className={styles.signinbutton}>
+    <button 
+      type="button" 
+      className={styles.signinbutton}
+      onClick={() => signOut()}
+    >
       <FaGithub color='#04d361'/>
-      Nome user
+      {session.user.name}
       <FiX color='#737380' className={styles.closeIcon}/>
     </button>
   ) : (
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,7 @@ import { SessionProvider } from 'next-auth/react'
 import Header from '../components/Header'
 import '../styles/global.scss'
 
-function MyApp({ Component, pageProps:{session, ...pageProps} }: AppProps) {
+function MyApp({ Component, pageProps:{ session, ...pageProps }}: AppProps) {
   return (
     <SessionProvider session={session}>
       <Header/>
