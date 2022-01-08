5ac674498fb3e8ce51f477a40d63d3fd64b8e4cb
Gustavo Colombo <gusttavocolombo@gmail.com>
2022-01-08 18:53:35 -0300

    checkout session stripe

--- src/components/SubcribeButton/index.tsx
+++ src/components/SubcribeButton/index.tsx
@@ -1,3 +1,6 @@
+import { signIn, useSession } from 'next-auth/react'
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss'
 
 interface SubscriptionButtonProps{
@@ -5,6 +8,28 @@ interface SubscriptionButtonProps{
 }
 
 export default function SubscribeButton({priceId}: SubscriptionButtonProps){
+
+  const session = useSession();
+
+  async function handleCreateCheckoutSession(){
+    if(!session){
+      signIn('github')
+      return
+    }
+
+    try{
+      const response = await api.post('/subscribe')
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({sessionId: sessionId});
+    }catch{
+      throw new Error('Impossível fazer checkout de compra')
+    }
+  }
+
   return(
     <button
       type="button"
